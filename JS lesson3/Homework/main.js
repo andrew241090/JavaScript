4b6ba9fdde03b4974576fafced56e5bffe654d7a
AndriyAndrusyshyn <andrusyshyn_a@hotmail.com>
2021-11-16 16:12:57 +0200

    Homework 3

---
+++ JS lesson3/Homework/main.js
@@ -0,0 +1,29 @@
+// --створити масив з:
+//     - з 5 числових значень
+// - з 5 стічкових значень
+// - з 5 значень стрічкового, числового та булевого типу
+// - та вивести його в консоль
+
+    // let number= [5,18,56,4,8];
+    // let sring= ['wood','fish','mirror','eclipse','tupe'];
+    // let diffrent= [false,'Ivan',75,true,3]
+    // console.log(number,sring,diffrent);
+
+// -- Створити пустий масив. Наповнити його будь-якими значеннями звертаючись до конкретного індексу. Вивести в консоль
+
+
+   // let empty = [];
+   //  empty[0]=1;
+   //  empty[1]="andriy";
+   //  empty[2]=true;
+   //  empty[3]=45;
+   //  empty[4]='volvo';
+   //  console.log(empty);
+
+let year=+prompt('Введіть число');
+if (year%2===0){
+    console.log('Високосний')
+}
+else {
+    console.log('Не високосний')
+}
